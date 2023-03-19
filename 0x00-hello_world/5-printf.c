cdd54f36a0f1dfc47f8fe2ad646353020ea06552
otirosemary58@gmail.com <Rosemary-Oti>
2023-03-19 04:51:15 -0700

    c update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line with  the printf function
+ * main - A program that prints a line with the print f function
  * Return: Always 0 (success)
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-Return (0);
+return (0);
 }
