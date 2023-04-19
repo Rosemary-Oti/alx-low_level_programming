8e022bee9961cbfc76b2b4d5d4b93fd0e81d7fc9
otirosemary58@gmail.com <Rosemary-Oti>
2023-04-19 15:15:28 -0700

    task o

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/*
+ * print_name - A function that prints a name
+ * @name: string to print
+ * @f: pointer to a function
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+
+		f(name);
+}
